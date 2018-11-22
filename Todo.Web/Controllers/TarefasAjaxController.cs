569e7a3e53c7852ef37d3612441672f40257965c
Bruno Duarte <desenvolvedor1@sistemasinfotec.com.br>
2018-11-22 17:54:29 -0200

    async

---
+++ Todo.Web/Controllers/TarefasAjaxController.cs
@@ -0,0 +1,58 @@
+﻿using System.Web.Mvc;
+using ToDo.Data.DAL;
+using ToDo.Domain.Tarefas;
+
+namespace Todo.Web.Controllers
+{
+    [RoutePrefix("task")]
+    public class TarefasAjaxController : Controller
+    {
+        private TarefaDAL _tarefaDal = new TarefaDAL();
+        private ListaDAL _listaDal = new ListaDAL();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult ObterTarefas()
+        {
+            var tarefas = _tarefaDal.ObterTarefas();
+            return Json(tarefas, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ObterListas()
+        {
+            return Json(_listaDal.ObterListas(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Create(Tarefa tarefa)
+        {
+            _tarefaDal.GravarTarefa(tarefa);
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Edit(Tarefa tarefa)
+        {
+            _tarefaDal.GravarTarefa(tarefa);
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            Tarefa tarefa = _tarefaDal.Deletar(id);
+            return Json("Ok", JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ObterTarefaPorId(int? id)
+        {
+            Tarefa tarefa = _tarefaDal.ObterTarefaPorId(id);
+            JsonResult jsonResult = Json(tarefa, JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
+        }
+    }
+}
--- Todo.Web/Scripts/Crud/Lista/Lista.js
+++ Todo.Web/Scripts/Crud/Lista/Lista.js
@@ -1,224 +1,161 @@
-﻿function criar_linha_grid(Dados) {
-    var ret =
-        '<tr data-id=' + Dados.Id + '>' +
-        '<td>' + Dados.Nome + '</td>' +
-        '<td>' + Dados.Observacao + '</td>' +
-        '<td>' +
-        '<a class="btn btn-success btn-excluir btn-sm" role="button" data-toggle="tooltip" data-placement="top" title="Adicionar"><i class="fa fa-plus"></i></a>' +
-        '<a class="btn btn-success btn-alterar btn-sm" role="button" data-toggle="tooltip" data-placement="top" title="Alterar Grupo" style="margin-right: 3px"><i class="fa fa-pencil"></i></a>' +
-        '<a class="btn btn-success btn-excluir btn-sm" role="button" data-toggle="tooltip" data-placement="top" title="Excluir Grupo"><i class="fa fa-trash"></i></a>' +
-        '</td>' +
-        '</tr>';
-
-    return ret;
-}
-
-function CarregarGrupoProduto(idselecionar) {
-
-    var btn = $(this),
-        id = 0,
-        url = urlgrupoproduto,
-        param = { 'id': id };
-
-    $.post(url, add_anti_forgery_token(param), function (response) {
-
-        if (response.length > 0) {
-            $('#lst_grupoproduto option').remove();
-            $('#lst_grupoproduto').append('<option value="">Selecione um Grupo</option>');
-            //Popula os options com os valores retornados em JSON
-            for (var i = 0; i < response.length; i++) {
-                $('#lst_grupoproduto').append('<option value="' + response[i].GrupoProdutoId + '"> ' + response[i].Nome + '</option>');
-            }
-            if (idselecionar > 0) {
-                $('#lst_grupoproduto').val(idselecionar);
-            }
-
-        }
-        else {
-            $('#lst_grupoproduto option').remove();
-            $('#lst_grupoproduto').append('<option value="">Selecione um Grupo</option>');
-        }
-
-    })
-        .fail(function () {
-            swal(
-                'Ops, Algo Aconteceu :(',
-                'Algum erro no momento da pesquisa de Grupo de Produtos, tente novamente daqui a pouco. :)',
-                'warning'
-            );
-        });
-}
-
-
-function abrir(Dados, _titulo) {
-
-    $("#CadEditLista").modal();
-
-    $("#lst_grupoproduto").val(Dados.GrupoProdutoId);
-
-    $('#Id').val(Dados.Id);
-
-    $('#TxtNome').val(Dados.Nome);
-
-    $('#TxtObservacao').val(Dados.Observacao);
-}
-
+﻿/// <reference path="../../moment.js" />
+/// <reference path="../../jquery-3.3.1.intellisense.js" />
+//Load Data in Table when documents is ready
 $(document).ready(function () {
+    loadData();
+});
 
-    var modal_cadastro = $('#modal_cadastro');
-
-    $("#BtnIncluir").click(function () {
-        abrir({}, 'Inclusão de Lista');
-    });
-
-    //$(document).on('click', '.btn-alterar', function () {
-    $("#BtnAlterar").click(function () {
-        var btn = $(this),
-            id = btn.closest('tr').attr('data-id'),
-            url = urlEdit,
-            param = { 'id': id };
-
-
-        $.post(url, add_anti_forgery_token(param), function (response) {
-            if (response) {
-                abrir(response, 'Alteração Listas');
-            }
-        })
-            .fail(function () {
-                swal(
-                    'Ops, Algo Aconteceu :(',
-                    'Algum erro no momento da gravação das suas alterações, tente novamente daqui a pouco. :)',
-                    'warning'
-                );
+//Load Data function
+function loadData() {
+    $.ajax({
+        url: "/ListasAjax/ObterListas",
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            var html = '';
+            $.each(result, function (key, item) {
+                html += '<tr>';
+                html += '<td>' + item.Nome + '</td>';
+                html += '<td>' + item.Observacao + '</td>';
+                html += '<td><a href="#" onclick="return getbyID(' + item.Id + ')">Edit</a> | <a href="#" onclick="Delele(' + item.Id + ')">Delete</a></td>';
+                html += '</tr>';
             });
-
+            $('#Listas').html(html);
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
     });
+}
+//Add Data Function 
+function Add() {
+    var res = validate();
+    if (res === false) {
+        return false;
+    }
+    var ListaObj = {
+        Id: $('#Id').val(),
+        Nome: $('#TxtNome').val(),
+        Observacao: $('#TxtObservacao').val()
+    };
+    $.ajax({
+        url: "/ListasAjax/Create",
+        data: JSON.stringify(ListaObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            loadData();
+            $('#myModal').modal('hide');
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
 
-    $(document).on('click', '.btn-excluir', function () {
+//Function for getting the Data Based upon Employee ID
+function getbyID(Id) {
+    $('#TxtNome').css('border-color', 'lightgrey');
+    $('#TxtObservacao').css('border-color', 'lightgrey');
+    $.ajax({
+        url: "/ListasAjax/ObterListarPorId/" + Id,
+        typr: "GET",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            $('#TxtNome').val(result.Nome);
+            $('#TxtObservacao').val(result.Observacao);
+            $('#Id').val(result.Id);
+            $('#myModal').modal('show');
+            $('#btnUpdate').show();
+            $('#btnAdd').hide();
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+    return false;
+}
 
-        var btn = $(this),
-            tr = btn.closest('tr'),
-            id = tr.attr('data-id'),
-            url = urlexclusao,
-            param = { 'id': id };
+//function for updating employee's record
+function Update() {
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
+    var ListaObj = {
+        Id: $('#Id').val(),
+        Nome: $('#TxtNome').val(),
+        Observacao: $('#TxtObservacao').val()
+    };
+    $.ajax({
+        url: "/ListasAjax/Edit",
+        data: JSON.stringify(ListaObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            loadData();
+            $('#myModal').modal('hide');
+            $('#EmployeeID').val("");
+            $('#Name').val("");
+            $('#Age').val("");
+            $('#State').val("");
+            $('#Country').val("");
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
 
-        swal({
-            title: 'Confirma a Exclusão?',
-            text: 'Caso confirme, a exclusão será definitiva.',
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Sim, quero deletar!',
-            cancelButtonText: 'Não'
-        }).then((result) => {
-            if (result.value) {
-                $.post(url, add_anti_forgery_token(param), function (response) {
-                    if (response.Resultado === "OK") {
-                        tr.remove();
-                        var quant = $('#grid_cadastro > tbody > tr').length;
-                        if (quant === 0) {
-                            $('#grid_cadastro').addClass('Ocultar');
-                            $('#mensagem_grid').removeClass('Ocultar');
-                        }
-                        swal({
-                            position: 'top-end',
-                            type: 'success',
-                            title: 'Operação efetuada com Sucesso',
-                            showConfirmButton: false,
-                            timer: 1500
-                        })
-                    }
-                    else if (response.Resultado === "ERRO") {
-                        formatar_mensagens_aviso(response.Mensagens, response.Resultado);
-                    }
-                    else if (response.Resultado === "AVISO") {
-                        formatar_mensagens_aviso(response.Mensagens, response.Resultado);
-
-                    }
-                })
-                    .fail(function () {
-                        swal(
-                            'Ops, Algo Aconteceu :(',
-                            'Algum erro no momento da exclusão, este item que você está tentando excluir pode estar vinculado a algum outro registro do sistema, ou ser o único com perfil de administrador.',
-                            'warning'
-                        );
-                    });
+//function for deleting employee's record
+function Delele(Id) {
+    var ans = confirm("Tem certeza que deseja deletar essa Lista de Tarefas");
+    if (ans) {
+        $.ajax({
+            url: "/ListasAjax/Delete/"+Id,
+            type: "POST",
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function (result) {
+                loadData();
+            },
+            error: function (errormessage) {
+                alert(errormessage.responseText);
             }
         });
+    }
+}
 
-    });
-
-
-    $("#btn_confirmar").click(function () {
-
-        var btn = $(this),
-            url = urlcadastro,
-            param = {
-                ProdutoId: $('#id_produto').val(),
-                GrupoProdutoId: $('#lst_grupoproduto').val(),
-                Nome: $('#txt_nome').val(),
-                NomeCardapio: $('#txt_nomecardapio').val(),
-                DescricaoCardapio: $('#txt_descricaocardapio').val(),
-                Preco: $('#txt_preco').val(),
-                Ativo: $('#chk_ativo').prop('checked'),
-                QuantidadeFracionada: $('#chk_quantidadefracionadaativo').prop('checked'),
-                AceitaOpcionais: $('#chk_aceitaopcionaisativo').prop('checked'),
-            };
-
-        var linha;
-        var msg_resposta;
-        $.post(url, add_anti_forgery_token(param), function (response) {
-            if (response.ProdutoId > 0) {
-                if (param.ProdutoId === 0 || param.ProdutoId === "") {
-                    param.ProdutoId = response.IdSalvo;
-                    var table = $('#grid_cadastro').find('tbody');
-                    linha = criar_linha_grid(response);
-                    table.append(linha);
-                    msg_resposta = "Inclusão efetuada com sucesso.";
-                }
-                else {
-                    linha = $('#grid_cadastro').find('tr[data-id=' + response.ProdutoId + ']').find('td');
-                    var cl_id = '<div class="rounded bg-info small" align="center" style="color:whitesmoke">' + response.ProdutoId + '</div>';
-                    linha.eq(0).html(cl_id).end();
-                    linha.eq(1).html(response.Nome).end();
-                    linha.eq(2).html(response.Preco).end();
-                    var cl_status = param.Ativo ? 'Ativo' : 'Desativado';
-                    linha.eq(3).html('<span class="badge badge-primary">' + cl_status + '</span>').end();
-                    var cl_status = param.QuantidadeFracionada ? 'Ativo' : 'Desativado';
-                    linha.eq(4).html('<span class="badge badge-primary">' + cl_status + '</span>').end();
-                    var cl_status = param.AceitaOpcionais ? 'Ativo' : 'Desativado';
-                    linha.eq(5).html('<span class="badge badge-primary">' + cl_status + '</span>').end();
-                    msg_resposta = "Alteração efetuada com sucesso.";
-                }
-                $('#telacadastroproduto').modal('hide');
-                swal({
-                    position: 'top-end',
-                    type: 'success',
-                    title: msg_resposta,
-                    showConfirmButton: false,
-                    timer: 1500
-                })
-
-
-            }
-            else if (response.Resultado === "ERRO") {
-                formatar_mensagens_aviso(response.Mensagens, response.Resultado);
-            }
-            else if (response.Resultado === "AVISO") {
-                formatar_mensagens_aviso(response.Mensagens, response.Resultado);
-
-            }
-
-        })
-            .fail(function () {
-                swal(
-                    'Ops, Algo Aconteceu :(',
-                    'Algum erro no momento da gravação das suas alterações, tente novamente daqui a pouco. :)',
-                    'warning'
-                );
-            });
-
-    });
-
-});
+//Function for clearing the textboxes
+function clearTextBox() {
+    $('#Id').val("");
+    $('#TxtNome').val("");
+    $('#TxtObservacao').val("");
+    $('#btnUpdate').hide();
+    $('#btnAdd').show();
+    $('#TxtNome').css('border-color', 'lightgrey');
+    $('#TxtObservacao').css('border-color', 'lightgrey');
+}
+//Valdidation using jquery
+function validate() {
+    var isValid = true;
+    if ($('#TxtNome').val().trim() == "") {
+        $('#TxtNome').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#TxtNome').css('border-color', 'lightgrey');
+    }
+    if ($('#TxtObservacao').val().trim() == "") {
+        $('#TxtObservacao').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#TxtObservacao').css('border-color', 'lightgrey');
+    }
+    return isValid;
+}
